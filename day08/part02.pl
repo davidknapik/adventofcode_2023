25fbfef9e1fbb89b52c10af5d94732c6896ec49b
David Knapik <david.knapik@gmail.com>
2023-12-08 12:25:24 -0700

    changed to find the cycle rate for each node, then finding LCM(cycle[n])

--- day08/part02.pl
+++ day08/part02.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl 
 
+
 # my $input_file = 'test_input1.txt' ;
 # my $input_file = 'test_input2.txt' ;
-# my $input_file = 'test_input3.txt' ;
-my $input_file = 'input.txt' ;
+# my $input_file = 'test_input3.txt' ; # 6
+my $input_file = 'input.txt' ;  # 12,315,788,159,977
+
 my $start_time = time;
 my @current_nodes ;
 
@@ -92,6 +94,7 @@ until ($exit == 1) {
 
         if ($current_nodes[$array_index2] =~ m/\w\w[Z]/) {
             $match++ ;
+            printf("Node_index: %d hit Z at step %d\n",$array_index2, $step_count );
         } else {
             $exit = 0 ;
         }
@@ -114,3 +117,19 @@ printf("\n");
 printf("Step Count: %d\n", $step_count);
 
 printf("Duration: %02d:%02d:%02d\n", (gmtime(time - $start_time))[2,1,0]) ;
+
+#
+# Found the frequency the patters for each repeates and then find LCM for the numbers
+#
+#       0	            1       		2       		3       		4       		5	
+#       BXA             KBA     		VTA     		AAA     		HMA     		HLA	
+#       13200	        22410	        18726	        18112		    16270		    20568	
+# 13201	26401	22411	44821	18727	37453	18113	36225	16271	32541	20569	41137	
+# 13201	39602	22411	67232	18727	56180	18113	54338	16271	48812	20569	61706	
+# 13201	52803	22411	89643	18727	74907	18113	72451	16271	65083	20569	82275	
+# 13201	66004	22411	112054	18727	93634	18113	90564	16271	81354	20569	102844	
+# 13201	79205	22411	134465	18727	112361	18113	108677	16271	97625	20569	123413	
+# 13201	92406	22411	156876	18727	131088	18113	126790	16271	113896	20569	143982	
+
+#
+#LCM( 13201, 22411, 18727, 18113, 16271, 20569 ) = 12,315,788,159,977
